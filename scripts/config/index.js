606774b03edc932946e445ba3b4feb3bbbd21db2
Hong.liu <hong.liu@mainiway.com>
2018-05-15 10:00:53 +0800

    feat: electron builder

--- scripts/config/index.js
+++ scripts/config/index.js
@@ -3,6 +3,6 @@ const path = require('path')
 module.exports = {
   dist: process.env.NODE_ENV !== 'production'
     ? path.resolve('./node_modules/.electorn-cache')
-    : path.resolve('./dist'),
+    : path.resolve('./build'),
   host: 'http://localhost:35829'
 }
